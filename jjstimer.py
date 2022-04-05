56ddee5791e2cd1ff0f91b2bf944794e6d052ea2
GoneSahlin <zach@sahlins.net>
2022-04-05 13:20:44 -0700

    added testing suite and fixed issue

--- jjstimer.py
+++ jjstimer.py
@@ -6,7 +6,7 @@ sandwich_names =["N/A","The Pepe","Big John","Totally Tuna",
                     "Turkey Tom","Vito","The Veggie","Spicy East Coast Italian","Billy Club","Italian Night Club","Hunter's Club","Country Club","Beach Club","Jimmy Cubano","Bootlegger Club","Club Tuna","Club LuLu","Ultimate Porker","JJ Gargantuan"]
 
 def findNextTime(curTime):
-    nextTime = datetime(curTime.year, curTime.month, curTime.day, 13, 0, 0, 0)
+    nextTime = datetime(curTime.year, curTime.month, curTime.day, 13, 40, 0, 0)
     while nextTime.weekday() not in [1, 3] or nextTime < curTime:
         nextTime += timedelta(days=1)
     return nextTime
@@ -20,12 +20,12 @@ def Sandwich_of_the_day():
             dashes = dashes + 1
         if dashes == 1:
             month = month + char
-        if dashes == 2: 
-            day = day + char 
+        if dashes == 2:
+            day = day + char
 
     month = (-1 *int(month))
     day = (-1 * int(day))
-    seed = month * day 
+    seed = month * day
     rd.seed(seed)
     temp = rd.randrange(1,18)
     print("Today's sandwich of the day is a #"+str(temp)+" "+sandwich_names[temp])
---
+++ test_jjstimer.py
@@ -0,0 +1,21 @@
+from jjstimer import findNextTime
+
+from datetime import datetime
+
+
+def test_findNextTime():
+    time_1 = datetime(2022, 4, 5, 10)
+    next_1 = findNextTime(time_1)
+    correct_1 = datetime(2022, 4, 5, 13, 40)
+    assert next_1 == correct_1
+    time_2 = datetime(2022, 4, 6, 13, 20)
+    next_2 = findNextTime(time_2)
+    correct_2 = datetime(2022, 4, 7, 13, 40)
+    assert next_2 == correct_2
+    time_3 = datetime(2022, 5, 31, 14)
+    next_3 = findNextTime(time_3)
+    correct_3 = datetime(2022, 6, 2, 13, 40)
+    assert next_3 == correct_3
+
+if __name__ == "__main__":
+    test_findNextTime()
