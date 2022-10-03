f8622b99827b4e800c3a7f96d36b67ed6900dc60
cbarker4 <cbarker4@zagmail.gonzaga.edu>
2022-10-03 14:39:20 -0700

    Changed time to mondays after depalma's

--- jjstimer.py
+++ jjstimer.py
@@ -6,12 +6,8 @@ sandwich_names =["N/A","The Pepe","Big John","Totally Tuna",
                     "Turkey Tom","Vito","The Veggie","Spicy East Coast Italian","Billy Club","Italian Night Club","Hunter's Club","Country Club","Beach Club","Jimmy Cubano","Bootlegger Club","Club Tuna","Club LuLu","Ultimate Porker","JJ Gargantuan"]
 
 def findNextTime(curTime):
-    nextTime = datetime(curTime.year, curTime.month, curTime.day, 13, 40, 0, 0)
-<<<<<<< HEAD
-=======
-   
->>>>>>> f81615a2ed67905e2d368a0b402ea7a5dd94ace2
-    while nextTime.weekday() not in [1, 3] or nextTime < curTime:
+    nextTime = datetime(curTime.year, curTime.month, curTime.day, 11, 50, 0, 0)
+    while nextTime.weekday() not in [0] or nextTime < curTime:
         nextTime += timedelta(days=1)
     return nextTime
 
